5f52574a6f74c844cef4db7c06dcaa5ae8c3a519
Carlos Barbosa <fbcarlos101@gmail.com>
2022-05-13 18:24:22 +0100

    [ADDED]: Function for option 5

--- tui/op5.c
+++ tui/op5.c
@@ -4,11 +4,27 @@
 #include "utils_tui.h"
 #include "funcs.h"
 
-void op5(int**bd){
+
+void create_user(){
+    char name[1024];
+    char address[1024];
     char opt[1024];
+
+    puts("\e[H\e[2J\e[3JSet name:");
+    fgets(name,1024,stdin);
+    newline_to_nullt(name);
+
+    puts("\e[H\e[2J\e[3JSet address:");
+    fgets(address,1024,stdin);
+    newline_to_nullt(address);
+    
     do {
-        printf("\e[H\e[2J\e[3JOP5\n\nQ) Go Back\n> ");
+        printf("\e[H\e[2J\e[3JName:%s\nAddress:%s\n\nQ) Go Back\n> ",name,address);
         fgets(opt,1024,stdin);
         newline_to_nullt(opt);
     } while(*opt!='Q' && *opt!='q');
+}
+
+void op5(int**bd){
+    create_user();
 }
